1929d28db94c3699de554c6c69ff50b6f69faeae
Maciej Serylak <mserylak@gmail.com>
2020-04-07 13:24:28 +0200

    Update template.py

--- schedule_blocks/template.py
+++ schedule_blocks/template.py
@@ -1,10 +1,10 @@
 configure_obs()
 obs.sb.new(owner='Maciej Serylak')
-obs.sb.description='TRAPUM session 3: Ter 5'
+obs.sb.description='TRAPUM session <NUMBER>: <TARGET>'
 obs.sb.antenna='available'
 obs.sb.controlled_resources_spec='cbf,sdp,apsuse,fbfuse,tuse'
-obs.sb.instruction_set="run-obs-script /home/kat/katsdpscripts/observation/track.py 'Ter 5,radec,,' -t 14400 --fbfuse-config-auth-host='thn00.tuse.mkat.karoo.kat.ac.za:5003'"
-obs.sb.desired_start_time=''
+obs.sb.instruction_set="run-obs-script /home/kat/katsdpscripts/observation/track.py '<TARGET>,radec,<HH:MM:SS.SS>,<DD:MM:SS.SS>' -t <T_OBS> --fbfuse-config-auth-host='thn00.tuse.mkat.karoo.kat.ac.za:5003'"
+obs.sb.desired_start_time='YYYY-MM-DD HH:MM:SS'
 obs.sb.proposal='SCI-20180923-MK-01'
 obs.sb.to_defined()
 obs.sb.to_approved()
