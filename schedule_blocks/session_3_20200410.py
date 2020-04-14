3cdfe901b4693ac16d42cf3138e9a3db1670fce1
Maciej Serylak <mserylak@gmail.com>
2020-04-14 10:05:46 +0200

    Bug fixes to schedule block.

--- schedule_blocks/session_3_20200410.py
+++ schedule_blocks/session_3_20200410.py
@@ -1,11 +1,14 @@
 configure_obs()
 obs.sb.new(owner='Maciej Serylak')
-obs.sb.description='TRAPUM session 3: Ter 5'
-obs.sb.antenna='available'
-obs.sb.controlled_resources_spec='cbf,sdp,apsuse,fbfuse'
-obs.sb.instruction_set="run-obs-script /home/kat/katsdpscripts/observation/track.py 'Ter 5,radec,17:48:04.80,-24:46:45' -t 14400 --fbfuse-config-auth-host='thn00.tuse.mkat.karoo.kat.ac.za:5003'"
-obs.sb.desired_start_time='2020-04-10 01:00:00'
-obs.sb.proposal='SCI-20180923-MK-01'
+obs.sb.description = 'TRAPUM session 3: Ter 5'
+obs.sb.antenna_spec = 'available'
+obs.sb.type = katuilib.ScheduleBlockTypes.OBSERVATION
+obs.sb.controlled_resources_spec = 'cbf,sdp,apsuse,fbfuse'
+obs.sb.instruction_set = "run-obs-script /home/kat/katsdpscripts/observation/track.py 'Ter 5,radec,17:48:04.80,-24:46:45' -t 14400 --fbfuse-config-auth host='thn00.tuse.mkat.karoo.kat.ac.za:5003'"
+obs.sb.desired_start_time = '2020-04-10 01:00:00'
+obs.sb.proposal_id = 'SCI-20180923-MK-01'
 obs.sb.to_defined()
 obs.sb.to_approved()
 obs.sb.unload()
+
+# This observations failed due to the issues with APUSE.
