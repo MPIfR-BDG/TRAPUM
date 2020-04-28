6ecec168922319d4685f688103f5e0ca2398736a
Maciej Serylak <mserylak@gmail.com>
2020-04-28 12:29:06 +0200

    Update and rename session_5_202004XX.py to session_5_20200502.py

--- schedule_blocks/session_5_20200502.py
+++ schedule_blocks/session_5_20200502.py
@@ -5,7 +5,7 @@ obs.sb.antenna_spec = 'available'
 obs.sb.type = katuilib.ScheduleBlockTypes.OBSERVATION
 obs.sb.controlled_resources_spec = 'cbf,sdp,apsuse,fbfuse,tuse'
 obs.sb.instruction_set = "run-obs-script /home/kat/katsdpscripts/observation/track.py '47 Tuc,radec,00:24:05.67,-72:04:52.6' -t 14400 --fbfuse-config-auth-host='thn00.tuse.mkat.karoo.kat.ac.za:5003'"
-obs.sb.desired_start_time = '2020-04-15 00:30:00'
+obs.sb.desired_start_time = '2020-05-02 06:00:00'
 obs.sb.proposal_id = 'SCI-20180923-MK-01'
 obs.sb.to_defined()
 obs.sb.to_approved()
